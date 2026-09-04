75664a711f74297a17d367e9520533d180f710ac
Richard Bateman <taxilian@gmail.com>
2026-09-03 21:43:21 -0600

    feat(app): remove old placeholder files from T1

--- BicTermUITests/BicTermUITests.swift
+++
@@ -1,9 +0,0 @@
-import XCTest
-
-final class BicTermUITests: XCTestCase {
-    func testAppLaunchesAndShowsPlaceholder() {
-        let app = XCUIApplication()
-        app.launch()
-        XCTAssertTrue(app.staticTexts["BicTerm"].waitForExistence(timeout: 10))
-    }
-}
